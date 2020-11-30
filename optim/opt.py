a395b5a78e9a742b7fbcf0bf9fe8a0f7022499a5
Fengfan <fengkai1996@foxmial.com>
2020-11-30 11:44:06 +0800

    add optim speed

--- optim/opt.py
+++ optim/opt.py
@@ -1,7 +1,8 @@
 import torch
 from matplotlib import pyplot as plt
 import numpy as np
-import random
+
+epoch = 50000
 
 def f(x,y):
     return (1.5-x+x*y)**2+(2.25-x+x*y*y)**2+(2.625-x+x*y*y*y)**2
@@ -25,11 +26,13 @@ def Adam(x, y, ax):
     X = []
     Y = []
     Z = []
-    while iters<8000:
+    loss = []
+    while iters<epoch:
         iters+=1
         X.append(x)
         Y.append(y)
         Z.append(f(x,y))
+        loss.append(f(x,y))
         m_x = b1*m_x + (1-b1)*hx(x,y)
         v_x = b2*v_x + ((1-b2)*hx(x,y))**2
         m_y = b1*m_y + (1-b1)*hy(x,y)
@@ -42,7 +45,7 @@ def Adam(x, y, ax):
         y = y - n/np.sqrt(v_het_y+e)*m_het_y
     print('adam的最终输出：', x, y, f(x,y))
     ax.plot3D(X,Y,Z,'orange',label='adam')
-
+    return loss
 
 
 def ada_grad(x, y, ax):
@@ -55,17 +58,20 @@ def ada_grad(x, y, ax):
     X = []
     Y = []
     Z = []
-    while iters<8000:
+    loss = []
+    while iters<epoch:
         iters+=1
         X.append(x)
         Y.append(y)
         Z.append(f(x,y))
+        loss.append(f(x,y))
         sum_x_grad += (hx(x,y))**2
         sum_y_grad += (hy(x,y))**2
         x = x - n/np.sqrt(sum_x_grad+e)*(hx(x,y))
         y = y - n/np.sqrt(sum_y_grad+e)*(hy(x,y))
     print('ada_grad的最终输出：',x, y,f(x,y))
     ax.plot3D(X,Y,Z,'black', label='ada_grad')
+    return loss
 
 def momentum(x, y, ax):
     # 学习率
@@ -79,18 +85,20 @@ def momentum(x, y, ax):
     X = []
     Y = []
     Z = []
-    while iters<8000:
+    loss = []
+    while iters<epoch:
         iters+=1
         X.append(x)
         Y.append(y)
         Z.append(f(x,y))
+        loss.append(f(x,y))
         vx = a*vx - n*hx(x,y)
         x = x + vx
         vy = a*vy - n*hy(x,y)
         y = y + vy
     print('momentum的最终输出：',x,y,f(x,y))
     ax.plot3D(X,Y,Z,'gray',label='momentum')
-    
+    return loss
 
 
 
@@ -105,18 +113,20 @@ def rmsprop(x, y, ax):
     Y = []
     Z = []
     a = 0.1
-    while iters<8000:
+    loss = []
+    while iters<epoch:
         iters+=1
         X.append(x)
         Y.append(y)
         Z.append(f(x,y))
+        loss.append(f(x,y))
         sum_x_grad = a*sum_x_grad + (1-a)*(hx(x,y))**2
         sum_y_grad = a*sum_y_grad + (1-a)*(hy(x,y))**2
         x = x - n/np.sqrt(sum_x_grad+e)*(hx(x,y))
         y = y - n/np.sqrt(sum_y_grad+e)*(hy(x,y))
     print('rmsprop的最终输出：',x, y,f(x,y))
     ax.plot3D(X,Y,Z,'blue', label='rmsprop')
-
+    return loss
 
 if __name__ == "__main__":
     fig = plt.figure()
@@ -130,10 +140,20 @@ if __name__ == "__main__":
     # 初值
     x = 2
     y = -1
-    momentum(x,y,ax)
-    ada_grad(x,y,ax)
-    Adam(x,y,ax)
-    rmsprop(x, y, ax)
+    loss_m = momentum(x,y,ax)
+    loss_ag = ada_grad(x,y,ax)
+    loss_am = Adam(x,y,ax)
+    loss_rm = rmsprop(x, y, ax)
     plt.legend(loc='upper left')
     plt.savefig('./optim/test.jpg')
-    pass
+    
+
+    x = np.linspace(0,epoch,num=epoch)
+    fig,ax = plt.subplots()
+    ax.plot(x,loss_m,label='momentum')
+    ax.plot(x,loss_am,label='adam')
+    ax.plot(x,loss_ag,label='ada_grad')
+    ax.plot(x,loss_rm,label='rmsprop')
+    ax.set_title('optim speed')
+    ax.legend()
+    plt.savefig('./optim/speed.jpg')
